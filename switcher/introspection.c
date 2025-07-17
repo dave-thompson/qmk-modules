7e03c2521691e54c445ec3fa3ec04d220b750faf
Dave Thompson <175743+dave-thompson@users.noreply.github.com>
2025-07-17 12:09:23 +0100

    docs: switcher - improve comments

--- switcher/introspection.c
+++ switcher/introspection.c
@@ -1,5 +1,17 @@
 #ifdef COMMUNITY_MODULE_SWITCHER_ENABLE
 
+/**
+ * switcher_seconday_keys_count(void): Returns the number of secondary keys specified in keymap.c
+ * 
+ * If you don't want to define your secondary keys in an array, as outlined
+ * in switcher.h, then simply define them in the way you wish and override
+ * BOTH switcher_secondary_keys_count() and switcher_secondary_keys_get()
+ * in your keymap.c file.
+ * 
+ * You may refer to the raw implementations below in your overriding methods
+ * if useful.
+ * 
+ */
 uint16_t switcher_secondary_keys_count_raw(void) {
   return ARRAY_SIZE(switcher_secondary_keys);
 }
@@ -8,6 +20,10 @@ __attribute__((weak)) uint16_t switcher_secondary_keys_count(void) {
   return switcher_secondary_keys_count_raw();
 }
 
+/**
+ * switcher_secondary_keys_get(uint16_t index): returns the key at index
+ * 
+ */
 const switcher_key_t* switcher_secondary_keys_get_raw(uint16_t index) {
   if (index >= switcher_secondary_keys_count_raw()) {
     return NULL;
