5936c3604129a471c9c9a6bca48d25c9b67572c7
Dave Thompson <175743+dave-thompson@users.noreply.github.com>
2025-07-25 12:30:07 +0100

    feat: switcher - auto-select on idle

--- switcher/switcher.c
+++ switcher/switcher.c
@@ -5,8 +5,12 @@ ASSERT_COMMUNITY_MODULES_MIN_API_VERSION(1, 0, 0);
 static bool switcher_active = false; // Is the switcher in use?
 static bool window_mode = false; // Are we in window switcher mode or app switcher mode?
 
-#define SWITCHER_BOOT_DURATION 150 // Estimated time in ms for the switcher _software_ to boot up after the SWITCHER keystroke is sent; during this time window, any keystrokes will be cached and then sent once boot is expected to have completed
-#define SWITCHER_WINDOWS_BOOT_DURATION 400 // Maximum time in ms for the switcher _software_ to load the window manager; during this time window, any keystrokes will be cached and then sent once loading is expected to have completed
+#define SWITCHER_IDLE_TIMEOUT 0 // Max time between keypresses before Switcher closes automatically, selectimg the highlighted item
+static uint16_t idle_timer = 0;
+static bool switcher_key_held = false;
+
+#define SWITCHER_BOOT_DURATION 200 // Estimated time in ms for the switcher _software_ to boot up after the SWITCHER keystroke is sent; during this time window, any keystrokes will be cached and then sent once boot is expected to have completed
+#define SWITCHER_WINDOWS_BOOT_DURATION 10000// 400 // Maximum time in ms for the switcher _software_ to load the window manager; during this time window, any keystrokes will be cached and then sent once loading is expected to have completed
 static uint16_t initial_boot_timer = 0; // Estimated switcher _software_ boot completion time; 0 if and only if app switcher not currently booting
 static uint16_t window_boot_timer = 0; // Estimated switcher _software_ window mode boot completion time; 0 if and only if window mode not currently booting
 
@@ -16,7 +20,7 @@ static uint16_t window_boot_timer = 0; // Estimated switcher _software_ window m
 static uint16_t key_cache[CACHE_SIZE];
 static uint8_t cache_count = 0;
 static keyrecord_t ending_record;
-static bool received_ending_record = false;
+static bool ending_record_cached = false;
 
 // Defined in introspection.c
 uint16_t switcher_secondary_keys_count(void);
@@ -42,6 +46,10 @@ static bool timer_just_ended(uint16_t *timer) {
     return false;
 }
 
+static void reset_idle_timer(void) {
+    begin_timer(&idle_timer, SWITCHER_IDLE_TIMEOUT);
+}
+
 static bool loading(void) {
     return initial_boot_timer || window_boot_timer;
 }
@@ -55,10 +63,24 @@ static void cache_keycode(uint16_t keycode) {
 
 static void exit_switcher(void) {
     unregister_code(SWITCHER_VIRTUAL_HOLD_KEY);
-    switcher_active = window_mode = received_ending_record = false;
+    switcher_active = window_mode = ending_record_cached = false;
+    cache_count = 0;
+}
+
+static void select_highlighted_item(void) {
+    if (window_mode) { // if window switcher in use: select the highlighted window
+        tap_code(KC_ENTER);
+        exit_switcher();
+    }
+    else { // app switcher in use: hold alt while exiting to instruct macos to open window even if minimised
+        register_code(KC_LALT);
+        exit_switcher();
+        unregister_code(KC_LALT);
+    }
 }
 
 static void process_secondary_key(uint16_t virtual_keycode) {
+    reset_idle_timer();
     if (loading()) {
         cache_keycode(virtual_keycode);
     }
@@ -84,24 +106,17 @@ static void process_secondary_key(uint16_t virtual_keycode) {
 static void process_ending_key(keyrecord_t *record) {
     if (loading()) { // if loading, cache the record for later processing
         ending_record = *record;
-        received_ending_record = true;
+        ending_record_cached = true;
     }
     else {
-        if (window_mode) { // if window switcher in use: select the highlighted window
-            tap_code(KC_ENTER);
-            exit_switcher();
-        }
-        else { // app switcher in use: hold alt while exiting to instruct macos to open window even if minimised
-            register_code(KC_LALT);
-            exit_switcher();
-            unregister_code(KC_LALT);
-        }        
+        select_highlighted_item();
         process_record(record); // Send the ending key for regular processing
     }
 }
 
 void process_cached_keys(void) {
-    // Check size of cache & prepare to re-cache keys if necessary (in case the processing of a cached key leads to an additional boot delay)
+    reset_idle_timer(); // in case idle timer expired during the preceding boot delay
+    // Prepare to re-cache keys if necessary (in case the processing of a cached key leads to an additional boot delay)
     uint16_t keys_to_process = cache_count;
     cache_count = 0;
     // Process any cached secondary keys
@@ -109,7 +124,7 @@ void process_cached_keys(void) {
         process_secondary_key(key_cache[i]);
     }
     // Process any cached ending key
-    if (received_ending_record) {
+    if (ending_record_cached) {
         process_ending_key(&ending_record);
     }
 }
@@ -126,8 +141,11 @@ bool process_record_switcher(uint16_t current_keycode, keyrecord_t *record) {
             }
             // tap the tap_key
             register_code(SWITCHER_VIRTUAL_TAP_KEY);
+            switcher_key_held = true;
         } else {
             unregister_code(SWITCHER_VIRTUAL_TAP_KEY);
+            switcher_key_held = false;
+            reset_idle_timer();
         }
         return false;
     } else if (switcher_active) { // switcher active; some key (other than primary trigger) pressed / released
@@ -160,5 +178,15 @@ void housekeeping_task_switcher(void){
     if (timer_just_ended(&window_boot_timer)) {
         tap_code(KC_RIGHT); // Select the first window automatically
         process_cached_keys();
+    }
+
+    // After Idle for SWITCHER_IDLE_TIMEOUT ms
+    if(timer_just_ended(&idle_timer)) {
+        // Automatically choose the highlighted item, unless the switcher key is held down, a new screen is booting, or there are cached keys.
+        //     (If neither boot timer is running, there are no cached keys.)
+        // If this conditional fails, the idle timer is reset when the switcher key is released or a boot timer expires.
+        if ((!switcher_key_held) && (!(initial_boot_timer || window_boot_timer))) {
+            select_highlighted_item();
+        }
     }
 }
