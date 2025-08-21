37cf5031fa7cdc3cc1d62d5d3f0f4f4fde754ac5
Dave Thompson <175743+dave-thompson@users.noreply.github.com>
2025-08-21 10:02:23 +0100

    Feat: Lumberjack - 50% lower firmware size (via custom string lib)

--- lumberjack/lumberjack.c
+++ lumberjack/lumberjack.c
@@ -1,21 +1,45 @@
 #if defined(CONSOLE_ENABLE) && defined(LUMBERJACK_LOG_KEYS)
 
 #include "quantum.h"
+#include "lumberjack_utils.h"
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Config Defaults
+//
+///////////////////////////////////////////////////////////////////////////////
 
 #ifndef LUMBERJACK_MAX_TRACKED_KEYS
     #define LUMBERJACK_MAX_TRACKED_KEYS 10 // one per finger
 #endif
 
-static uint16_t last_time = 0;
-static bool first_record = true;
+#ifndef LUMBERJACK_KEYCODE_LENGTH
+    #define LUMBERJACK_KEYCODE_LENGTH 15 // 6-char modifier, 7-char basic keycode, & 2 parens
+#endif
+
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Duration Calculation
+// State
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-// Reurns duration of keypress if this was a key release
+typedef struct {
+    bool active;                // Has there been a keypress in the last 60 secs?
+    uint16_t last_event_time;   // Time of last key event
+
+} lumberjack_state_t;
+
+static lumberjack_state_t state = {0};
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Calculating Hold Durations
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// Reurns duration of keypress, only if this was a key release
 // Returns 0 if this is a press (rather than release)
 // Returns 0 if this is a release but the press wasn't recorded (presumably as MAC_TRACKED_KEYS exceeded)
 // @note For accurate durations, all key presses must be passed to this function as well as all key releases
@@ -65,54 +89,62 @@ uint16_t get_duration(uint16_t keycode, keyrecord_t *record) {
 ///////////////////////////////////////////////////////////////////////////////
 
 // Handles all key presses/releases
-bool process_record_lumberjack(uint16_t current_keycode, keyrecord_t *record) {
+bool pre_process_record_lumberjack(uint16_t current_keycode, keyrecord_t *record) {
 
     uint16_t current_time = record->event.time;    
     uint16_t hold_duration = get_duration(current_keycode, record);
 
     // create pretty keycode string
-    char keycode_string[15+1];
+    char keycode_string[LUMBERJACK_KEYCODE_LENGTH+1];
     #ifdef KEYCODE_STRING_ENABLE
-        snprintf(keycode_string, 15+1, "%15s", get_keycode_string(current_keycode));
+        right_align_string(keycode_string, LUMBERJACK_KEYCODE_LENGTH+1, get_keycode_string(current_keycode));
     #else
         char hex_string[6+1];
-        snprintf(hex_string, 6+1, "0x%04X", current_keycode);
-        snprintf(keycode_string, 15+1, "%15s", hex_string);
+        keycode_to_hex_string(hex_string, current_keycode);
+        right_align_string(keycode_string, LUMBERJACK_KEYCODE_LENGTH+1, hex_string);
     #endif
 
     // log the key event
-    if (first_record == true) {
-        uprintf(" %s  |  DOWN  |                   |\n", keycode_string);
-        first_record = false;
-    } else
-        {
-            // calculate delta
-            uint16_t delta = current_time - last_time;
+    if (!state.active) {
+        uprintf(" %s  |  DOWN  |                    |\n", keycode_string);
+        state.active = true;
+    } else {
+        // calculate delta
+        uint16_t delta = current_time - state.last_event_time;
 
-            // pad delta for pretty printing
-            char delta_string[5+1];
-            if (delta > 60000)
-                snprintf(delta_string, 5+1, "%5d", delta); // if over 60000, assume negative
-            else
-                snprintf(delta_string, 5+1, "%5u", delta);
+        // convert delta to a string
+        char delta_string[6+1]; // 5 digits, 1 minus sign, 1 terminating NULL
+        if (delta > 61000) { // if well over 60s, must be negative
+            // note: negative deltas only arise in process_record, not in pre_process_record
+            delta = -(int)delta;
+            uint_to_string(delta_string, (int)delta);
+            prepend_char(delta_string, '-');
+        }
+        else
+            uint_to_string(delta_string, delta);
+        
+        // pad delta string for prettier printing
+        char padded_delta_string[6+1];
+        right_align_string(padded_delta_string, 6+1, delta_string);
 
+        // write to log
         if (record->event.pressed) {
-            uprintf(" %s  |  DOWN  |  Delta: %s ms  |\n", keycode_string, delta_string);
+            uprintf(" %s  |  DOWN  |  Delta: %s ms  |\n", keycode_string, padded_delta_string);
         } else {
-            uprintf(" %s  |  UP    |  Delta: %s ms  |  Hold: %u ms\n", keycode_string, delta_string, hold_duration);
+            uprintf(" %s  |  UP    |  Delta: %s ms  |  Hold: %u ms\n", keycode_string, padded_delta_string, hold_duration);
         }
     }
 
-    last_time = current_time;
+    state.last_event_time = current_time;
 
 return true;
 }
 
+// Stop clock if idle (over 60 seconds from last key event)
 void housekeeping_task_lumberjack(void) {
-    // reset if over 60 seconds from last key event
-    if (first_record == false) {
-        uint16_t delta = timer_read() - last_time;
-        if (delta > 60000 && delta < 62000) first_record = true;
+    if (state.active) {
+        uint16_t delta = timer_read() - state.last_event_time;
+        if (delta > 60000 && delta < 62000) state.active = false;
     }
 }
 
