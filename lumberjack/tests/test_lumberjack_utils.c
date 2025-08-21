308d77cf51f8e051c3636743b597653bf3002fc4
Dave Thompson <175743+dave-thompson@users.noreply.github.com>
2025-08-21 14:00:39 +0100

    Tests: Lumberjack - Added tests for utils library

---
+++ lumberjack/tests/test_lumberjack_utils.c
@@ -0,0 +1,70 @@
+#include "unity/unity.h"
+#include "../lumberjack_utils.h"
+#include <string.h>
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_str_len_should_return_correct_length(void) {
+    TEST_ASSERT_EQUAL(0, lumberjack_str_len(""));
+    TEST_ASSERT_EQUAL(10, lumberjack_str_len("lumberjack"));
+    TEST_ASSERT_EQUAL(1, lumberjack_str_len("1"));
+}
+
+void test_right_align_string_should_pad_correctly(void) {
+    char buffer[14+1];
+
+    lumberjack_right_align_string(buffer, 14+1, "lumberjack");
+    TEST_ASSERT_EQUAL_STRING("    lumberjack", buffer);
+}
+
+void test_right_align_string_should_truncate_long_strings(void) {
+    char buffer[7+1];
+
+    lumberjack_right_align_string(buffer, 7+1, "lumberjack");
+    TEST_ASSERT_EQUAL_STRING("berjack", buffer);
+}
+
+void test_prepend_char_should_add_character_at_start(void) {
+    char buffer[10+1] = "umberjack";
+    
+    lumberjack_prepend_char(buffer, 'l');
+    TEST_ASSERT_EQUAL_STRING("lumberjack", buffer);
+}
+
+void test_keycode_to_hex_string_should_format_correctly(void) {
+    char buffer[6+1];
+    
+    lumberjack_keycode_to_hex_string(buffer, 0x1234);
+    TEST_ASSERT_EQUAL_STRING("0x1234", buffer);
+    
+    lumberjack_keycode_to_hex_string(buffer, 0xABCD);
+    TEST_ASSERT_EQUAL_STRING("0xABCD", buffer);
+}
+
+void test_uint_to_string_should_convert_correctly(void) {
+    char buffer[5+1];
+    
+    lumberjack_uint_to_string(buffer, 0);
+    TEST_ASSERT_EQUAL_STRING("0", buffer);
+    
+    lumberjack_uint_to_string(buffer, 1038);
+    TEST_ASSERT_EQUAL_STRING("1038", buffer);
+    
+    lumberjack_uint_to_string(buffer, 65535);
+    TEST_ASSERT_EQUAL_STRING("65535", buffer);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    
+    RUN_TEST(test_str_len_should_return_correct_length);
+    RUN_TEST(test_right_align_string_should_pad_correctly);
+    RUN_TEST(test_right_align_string_should_truncate_long_strings);
+    RUN_TEST(test_prepend_char_should_add_character_at_start);
+    RUN_TEST(test_keycode_to_hex_string_should_format_correctly);
+    RUN_TEST(test_uint_to_string_should_convert_correctly);
+    
+    return UNITY_END();
+}
